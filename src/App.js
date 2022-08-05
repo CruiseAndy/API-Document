911cfcd4b19f33a4ca8e0cf8aff1fd982f5fd7b7
taylor <user@MacBook-Pro.local>
2022-08-05 18:05:17 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,80 @@
 /**
- * tool
+ * tools
  */
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 /**
- * pages
+ * style
  */
-import Document from './pages/document';
+import './index.css';
 
-const App = () => {
+const documentList = [
+  {
+    name: 'JP',
+    link: 'http://agocw.com/pay_docs/JP.html'
+  },
+  {
+    name: 'UA 印尼',
+    link: 'http://agocw.com/pay_docs/UA/ID.html'
+  },
+  {
+    name: 'UA 巴西',
+    link: 'http://agocw.com/pay_docs/UA/BR.html'
+  },
+  {
+    name: 'UA 印度',
+    link: 'http://agocw.com/pay_docs/UA/IN.html'
+  },
+  {
+    name: 'Change Log',
+    link: 'http://agocw.com/pay_docs/change_log.html'
+  }
+];
+
+const Document = () => {
+
+  /* state */
+  const [nowPage, setNowPage] = useState(null);
+
+  /* effect */
+  useEffect(() => {
+    setNowPage(documentList[0].link);
+  }, [])
+
+  /* function */
+  const onChangePage = pageLink => {
+    setNowPage(null);
+
+    setTimeout(() => setNowPage(pageLink), 500);
+  }
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/document" element={<Document />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="document_container">
+      <div className="document_menu_section">
+        <div className="document_caption">
+          <span>API Document</span>
+        </div>
+        <div className="document_menu_block">
+          {
+            documentList.map((item, idx) => {
+              const { name, link } = item;
+
+              return <p key={idx} className="menu_link" onClick={() => onChangePage(link)}>{name}</p>
+            })
+          }
+        </div>
+      </div>
+      <div className="document_content_section">
+        {
+          nowPage &&
+          <iframe
+            src={nowPage}
+            style={{ width: "100%", height: "100%", border: 0 }}
+          />
+        }
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default Document;
